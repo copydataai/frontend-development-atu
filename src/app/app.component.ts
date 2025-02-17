6585136a554f32a81e18764964764d5ab5199ffd
Jose Sanchez <engineer@copydataai.com>
2025-02-17 09:59:51 +0000

    feat: add component A & B exercise 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// importing component A and B from the ts file
+import { ComponentaComponent } from './componenta/componenta.component';
+import { ComponentbComponent } from './componentb/componentb.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // inject component A and component B
+  imports: [RouterOutlet, ComponentaComponent, ComponentbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // declaring name and age for reactivity by vars
   name = "Jose"
   age = 50
 }
