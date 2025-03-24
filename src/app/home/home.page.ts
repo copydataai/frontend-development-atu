997f2ba6c69a60e078a14ecb6157757b7aff1634
Jose Sanchez <engineer@copydataai.com>
2025-03-24 10:05:45 +0000

    feat(home): add movies "war" list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle } from '@ionic/angular/standalone';
+import { MoviesService } from "../services/movies.service";
+import { OnInit } from "@angular/core"
+
+export interface MovieI {
+  Title: string;
+  Type: string
+  Year: string;
+  Poster: string;
+  imdbID: string;
+}
+
+export interface MoviesSearchI {
+  Search: MovieI[];
+  totalResults: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  myMovies: MovieI[] = [];
+  constructor(private moviesService: MoviesService) { }
+
+  ngOnInit() {
+    this.getWarMovies();
+  }
+
+  getWarMovies() {
+
+    const war = this.moviesService.getMovies("war").subscribe(
+      (data: MoviesSearchI) => {
+        console.log(data)
+        this.myMovies = data.Search
+      }
+    );
+    console.log(war)
+  }
 }
