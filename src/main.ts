17c17ba603799027d9fd6098fb6d2c3918a08e99
Jose Sanchez <engineer@copydataai.com>
2025-04-06 23:13:12 +0100

    feat(main): add ionic storage

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { enableProdMode, importProvidersFrom } from "@angular/core"
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { IonicStorageModule } from "@ionic/storage-angular"
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -17,5 +19,6 @@ bootstrapApplication(AppComponent, {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
           }),
+    importProvidersFrom(IonicStorageModule.forRoot()),
   ],
 });
