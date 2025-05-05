02b91d9767974ff28daa7ed7b991efd08a9871b6
Jose Sanchez <engineer@copydataai.com>
2025-05-05 17:36:13 +0100

    feat(app): add landing home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,44 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle } from '@ionic/angular/standalone';
-import { MoviesService } from "../services/movies.service";
-import { OnInit } from "@angular/core"
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router'  ;
 
-export interface MovieI {
-  Title: string;
-  Type: string
-  Year: string;
-  Poster: string;
-  imdbID: string;
-}
-
-export interface MoviesSearchI {
-  Search: MovieI[];
-  totalResults: number;
-}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle, IonButton, IonIcon, RouterLink],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  myMovies: MovieI[] = [];
-  constructor(private moviesService: MoviesService) { }
-
-  ngOnInit() {
-    this.getWarMovies();
-  }
-
-  getWarMovies() {
-
-    const war = this.moviesService.getMovies("war").subscribe(
-      (data: MoviesSearchI) => {
-        console.log(data)
-        this.myMovies = data.Search
-      }
-    );
-    console.log(war)
-  }
 }
