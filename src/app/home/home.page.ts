3c2f77dcf5d93ac053762ecdfadc9f74decaa8b9
Jose Sanchez <engineer@copydataai.com>
2025-05-05 19:28:02 +0100

    fix(assets): remove unnecesary imports

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle, IonButton, IonIcon } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router'  ;
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle, IonButton, IonIcon, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,  IonButton, IonIcon, RouterLink],
 })
 export class HomePage {
 
